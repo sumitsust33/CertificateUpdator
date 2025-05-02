7ffdbe873efafe247fb09c9a44e1b930e6ec44ef
sumitsust33 <sumit.sust.33@gmail.com>
2025-05-02 14:48:49 +0600

    handled exception in test class

--- src/test/java/com/solvians/showcase/CertificateUpdateCallableTest.java
+++ src/test/java/com/solvians/showcase/CertificateUpdateCallableTest.java
@@ -1,20 +1,26 @@
 package com.solvians.showcase;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 public class CertificateUpdateCallableTest {
 
+    private CertificateUpdateCallable callable;
+
+    @BeforeEach
+    void setUp() {
+        callable = new CertificateUpdateCallable();
+    }
+
     @Test
     public void testCertificateUpdateCallable() {
-        CertificateUpdateCallable callable = new CertificateUpdateCallable();
-
         try {
             CertificateUpdate certificateUpdate = callable.call();
             CertificateUpdateTest certificateUpdateTest = new CertificateUpdateTest();
             certificateUpdateTest.testCertificateUpdate(certificateUpdate);
 
         } catch (Exception e) {
-
+            throw new RuntimeException("Failed to get certificate update", e);
         }
     }
 }
